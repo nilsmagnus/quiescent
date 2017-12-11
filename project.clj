8143be91814dcac1d41e12cd4bfedca6730f25e5
Nils Larsgård <nilsmagnus@gmail.com>
2017-12-11 22:02:19 +0100

    Upgrade react, clojure and clojurescript. Remove deprecated react-addons.

--- project.clj
+++ project.clj
@@ -3,8 +3,7 @@
   :url "http://github.com/levand/quiescent"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.7.228"]
-                 [cljsjs/react-with-addons "15.1.0-0"]
-                 [cljsjs/react-dom "15.1.0-0" :exclusions [cljsjs/react]]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/clojurescript "1.9.946"]
+                 [cljsjs/react-dom "16.2.0-0" :exclusions [cljsjs/react]]]
   :source-paths ["src"])
